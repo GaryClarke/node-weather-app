7b5a654e52fa33c2ee0882db54c0b18fcc1288f1
Gary Clarke <gary@pack-net.co.uk>
2019-09-24 08:41:15 +0100

    Remove unnecessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Clientside js is loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
