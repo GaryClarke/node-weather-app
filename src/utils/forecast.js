e15f8cc5c3c2bcfb8ed97eee2ebf820735e5ddc5
Gary Clarke <gary@pack-net.co.uk>
2019-09-24 08:38:03 +0100

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,27 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+
+    const url = 'https://api.darksky.net/forecast/936357ad9d673a955c4e84d4adae2f8c/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, {body}) => {
+
+        if (error) {
+
+            callback('Unable to connect to weather service.', undefined)
+
+        
+        } else if (body.error) {
+
+            callback('Unable to find location.', undefined)            
+
+        } else {
+
+            const currrentWeather = body.currently;
+
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${currrentWeather.temperature} degrees out. There is a ${(currrentWeather.precipProbability * 100).toFixed(0)}% chance of rain.`)    
+        }
+    })
+}
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,28 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZ2FyeWNsYXJrZTE5NzQiLCJhIjoiY2p6OHNmN2RxMDJmYjNjcGZ5cjVjMGl2OCJ9.meen1lAnpd6YlLAcLBGuuQ&limit=1'
+
+    request({ url, json: true }, (error, {body}) => {
+
+        if (error) {
+
+            callback('Unable to contact the geocoding service.', undefined)
+
+        } else if (body.features.length === 0) {
+
+            callback('Unable to find location. Try another search.', undefined)
+
+        } else {
+
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })    
+        }
+    })
+}
+
+module.exports = geocode
